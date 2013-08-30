26337e09053e7cb48524785fdf61f92168800a5e
george <rpubaddr0@gmail.com>
2013-08-30 17:05:16 -0600

    Normalized docstrings in httpserver.py.

--- objects/httpserver.py
+++ objects/httpserver.py
@@ -1,9 +1,9 @@
 """
-   NetworkObject.HTTPServer
-   This subclass extends NetworkObjects to provide functionality for
-   checking the availability of HTTP web services. You can instantiate
-   objects from this directly, or further subclass it to suit your
-   needs.
+NetworkObject.HTTPServer
+This subclass extends NetworkObjects to provide functionality for
+checking the availability of HTTP web services. You can instantiate
+objects from this directly, or further subclass it to suit your
+needs.
 """
 
 class HTTPServer(NetworkObject):
