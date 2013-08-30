633ff8c965f6aa469b1bdb0413a5ccfb06f3c1de
george <rpubaddr0@gmail.com>
2013-08-30 16:40:07 -0600

    Finished ping member functions in NetworkObject.
    Updated python-ping.

--- networkobject.py
+++ networkobject.py
@@ -10,7 +10,7 @@ class NetworkObject():
    
     def __init__():
         # Constructor
-        _pingResponse = Null
+        self._pingResponse = Null
 
     def ping(self):
         """
@@ -22,25 +22,20 @@ class NetworkObject():
         This function also sets/changes the value of self.pingResponse
         to include information about the ICMP response packet. This
         information can be accessed publicly in the form of a getter
-        member function that returns a dictionary:
-
-        >>> myNetworkObject.ping()
-        >>> myNetworkObject.getPingResponse()
-        {'delay': delay, 'ip': ip, 'packet_size': packet_size, 'ip_header': ip_header, 'icmp_header': icmp_header}
-        >>> myNetworkObject.getPingResponse().['delay']
-        delay
+        member function that returns a dictionary. See
+        getPingResponse().
 
         NOTE: Pinging in Python will open a raw socket, and requires
         root/Administrator permissions. The Ping class will raise
         socket.error if you don't have the proper permissions.
         """
         ping = Ping(self.ip)
-        ping.do()
-        # TODO: Fix python-ping so that it returns useful information
-        # and enerally acts more like a publicly accessable function
-        # rather than a part of a command-line interface.
-        # FIXME: Blindly returning False.
-        return False
+        self._pingResponse = ping.do()
+        
+        if self._pingResponse == Null:
+            return False
+        else:
+            return True
 
     def getPingResponse():
         """
@@ -50,5 +45,13 @@ class NetworkObject():
         self.ping() has not already been called, this function will
         return the default value of the object used to store ping
         response information, Null.
+
+        >>> myNetworkObject.getPingResponse()
+        Null
+        >>> myNetworkObject.ping()
+        >>> myNetworkObject.getPingResponse()
+        {'delay': delay, 'ip': ip, 'packet_size': packet_size, 'ip_header': ip_header, 'icmp_header': icmp_header}
+        >>> myNetworkObject.getPingResponse()['delay']
+        delay
         """
         return _pingResponse
