f4e0b413917e917d2dd8471df45fdb87874ddbbc
george <rpubaddr0@gmail.com>
2013-08-30 16:44:55 -0600

    Make httpserver.py more Pythonic.
    Renamed HttpServer to HTTPServer.

--- objects/httpserver.py
+++ objects/httpserver.py
@@ -1,10 +1,10 @@
 """
-   NetworkObject.HttpServer
+   NetworkObject.HTTPServer
    This subclass extends NetworkObjects to provide functionality for
    checking the availability of HTTP web services. You can instantiate
    objects from this directly, or further subclass it to suit your
    needs.
 """
 
-class HttpServer(NetworkObject):
+class HTTPServer(NetworkObject):
 
