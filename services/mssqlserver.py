3a1d60026b06cc08db16db5411bf36144112dada
george <rpubaddr0@gmail.com>
2013-10-04 11:13:39 -0600

    MS/pg should work now, but have not been tested.

--- services/mssqlserver.py
+++ services/mssqlserver.py
@@ -5,14 +5,27 @@ Microsoft SQL servers.
 """
 
 from services.sqlserver import SQLServer
+import pymssql
 
 class MSSQLServer(SQLServer):
-    def __init__(self, host, port=1433, user=None, password=None):
+    def __init__(self, host, port=1433, user=None, password=None,
+            database=None):
         super(MSSQLServer, self).__init__(host, port, user, password)
+        self.database = database
 
     def getStatus(self, **kwargs):
         """
-        Returns a boolean value depending on
+        Returns a boolean value depending on whather a successful login
+        can be performed on the server.
         """
         super(MSSQLServer, self).getStatus()
-        return None
+        
+        try:
+            conn = pymssql.connect(host=self.host, user=self.user,
+                    password=self.password, database=self.database)
+            cur = conn.cursor()
+            conn.close()
+        except:
+            return False
+
+        return True
--- services/pgsqlserver.py
+++ services/pgsqlserver.py
@@ -8,18 +8,24 @@ from services.sqlserver import SQLServer
 import psycopg2
 
 class PGSQLServer(SQLServer):
-    def __init__(self, host, port=5432, user=None, password=None):
+    def __init__(self, host, port=5432, user=None, password=None,
+            database=None):
         super(PGSQLServer, self).__init__(host, port, user, password)
+        self.database = database
 
     def getStatus(self, **kwargs):
         """
-        Returns a boolean value depending on
+        Returns a boolean value depending on whether a successful login
+        can be performed on the server.
         """
         super(PGSQLServer, self).getStatus()
 
         try:
             conn = psycopg2.connect(host=self,host, port=self.port,
-                    user=self.user, password=self.password)
+                    user=self.user, password=self.password,
+                    database=self.database)
+            cur = conn.cursor()
+            conn.close()
         except:
             return False
 
